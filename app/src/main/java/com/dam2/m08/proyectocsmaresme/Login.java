ca88ef314a4eee3099ff87939110f6bddd4b2efd
Cristopher Alonso <30374532l@iespoblenou.org>
2023-02-06 19:32:54 +0100

    funciones de registro y login correctos, añadido activity home.java y home.xml

--- app/src/main/java/com/dam2/m08/proyectocsmaresme/Login.java
+++ app/src/main/java/com/dam2/m08/proyectocsmaresme/Login.java
@@ -1,12 +1,17 @@
 package com.dam2.m08.proyectocsmaresme;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,11 +19,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Arrays;
+
 public class Login extends AppCompatActivity {
 
     private EditText usuario_login;
     private EditText contraseña_login;
     private Button btn_login;
+    private TextView enlaceRegistro;
+    private final String TAG ="PROYECTO_CS_MARESME___LOGIN";
+
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -26,12 +36,16 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
         setTitle("Login");
 
-        setup();
-    }
-    private void setup(){
         usuario_login = findViewById(R.id.usuario_login);
         contraseña_login = findViewById(R.id.contraseña_login);
         btn_login = findViewById(R.id.btn_login);
+        enlaceRegistro = findViewById(R.id.enlaceRegistro);
+        enlaceRegistro.setText("¿Ya tienes una cuenta? Registrate");
+
+        setup();
+    }
+    private void setup(){
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,16 +56,40 @@ public class Login extends AppCompatActivity {
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()){
-                                        //funcion o intent que manda al inicio de la app
+                                    if (!task.isSuccessful()){
+                                        showError(task.getException().getMessage());
+                                        Log.d(TAG, "task---: " +task.getException().getMessage());
                                     }
                                     else {
-                                        //mostrar error si algo sale mal con un dialog o toast
+                                        Intent intent = new Intent(Login.this, Home.class);
+                                        startActivity(intent);
                                     }
                                 }
                             });
                 }
             }
         });
+
+        enlaceRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void showError(String mensaje){
+        AlertDialog.Builder alert= new AlertDialog.Builder(this);
+        alert.setTitle("Error");
+        alert.setMessage(mensaje);
+        alert.setCancelable(false);
+        alert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Login.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        alert.create().show();
     }
 }
--- app/src/main/java/com/dam2/m08/proyectocsmaresme/Register.java
+++ app/src/main/java/com/dam2/m08/proyectocsmaresme/Register.java
@@ -1,12 +1,15 @@
 package com.dam2.m08.proyectocsmaresme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +22,7 @@ public class Register extends AppCompatActivity {
     private EditText usuario_register;
     private EditText contraseña_register;
     private Button btn_register;
+    private final String TAG ="PROYECTO_CS_MARESME___REGISTER";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,18 +47,36 @@ public class Register extends AppCompatActivity {
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()){
-                                        //funcion o intent para enviar al login y que se logue el usuario recien creado
-                                        Intent intent = new Intent(Register.this, Login.class);
-                                        startActivity(intent);
+                                    if (!task.isSuccessful()){
+                                        showError(task.getException().getMessage());
+                                        Log.d(TAG, "task---: " +task.getException().getMessage());
+                                        Log.d(TAG, "task---: " +task.getException().getLocalizedMessage());
+                                        Log.d(TAG, "task---: " +task.getException().getCause());
+                                        Log.d(TAG, "task---: " +task.getException().getSuppressed());
+                                        Log.d(TAG, "task---: " +task.getException().getStackTrace());
                                     }
                                     else {
-                                        //muestra el error en un toast o un dialog
+                                        Intent intent = new Intent(Register.this, Login.class);
+                                        startActivity(intent);
                                     }
                                 }
                             });
                 }
             }
         });
+    }
+    private void showError(String mensaje){
+        AlertDialog.Builder alert= new AlertDialog.Builder(this);
+        alert.setTitle("Error");
+        alert.setMessage(mensaje);
+        alert.setCancelable(false);
+        alert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Register.this, Register.class);
+                startActivity(intent);
+            }
+        });
+        alert.create().show();
     }
 }
