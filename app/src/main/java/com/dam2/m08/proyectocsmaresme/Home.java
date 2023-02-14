9b58e38944474453f7206f19cf61d2d9062d5c36
39446104t <39446104t@iespoblenou.org>
2023-02-14 16:07:34 +0100

    Creado el menú de Home

--- app/src/main/java/com/dam2/m08/proyectocsmaresme/Home.java
+++ app/src/main/java/com/dam2/m08/proyectocsmaresme/Home.java
@@ -1,9 +1,18 @@
 package com.dam2.m08.proyectocsmaresme;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.IOException;
 
 public class Home extends AppCompatActivity {
     @Override
@@ -11,4 +20,27 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = getIntent();
+        switch (item.getItemId()){
+            case R.id.games:
+                intent = new Intent(Home.this, Games.class);
+                startActivity(intent);
+                break;
+            case R.id.chat:
+                intent = new Intent(Home.this, Chat.class);
+                startActivity(intent);
+                break;
+            case R.id.configuration:
+                intent = new Intent(Home.this, Configuration.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
