5382902913e6e0ee6b434aed6c16419e3feec6e7
39446104t <39446104t@iespoblenou.org>
2023-02-14 16:39:31 +0100

    Creado el layout de chat

--- app/src/main/java/com/dam2/m08/proyectocsmaresme/Chat.java
+++ app/src/main/java/com/dam2/m08/proyectocsmaresme/Chat.java
@@ -18,7 +18,7 @@ public class Chat extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.chat);
     }
 
 
