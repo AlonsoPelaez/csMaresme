cf7cc6e5d9b11421e4ee5d122985ff76e8338eba
adriku32 <39446104t@iespoblenou.org>
2023-02-22 23:42:42 +0100

    Creadas las funciones de Report

--- app/src/main/java/com/dam2/m08/proyectocsmaresme/Report.java
+++ app/src/main/java/com/dam2/m08/proyectocsmaresme/Report.java
@@ -1,16 +1,35 @@
 package com.dam2.m08.proyectocsmaresme;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Report extends AppCompatActivity {
 
+    private EditText editTextReport;
+    private Button SendButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report);
         Toast.makeText(this, "Report", Toast.LENGTH_SHORT).show();
+
+        SendButton = findViewById(R.id.SendButton);
+
+        SendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Setting.class));
+            }
+        });
+
     }
 }
